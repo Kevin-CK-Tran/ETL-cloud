8a89a3434090a53f85d28a9855a8df9bff8ea32a
Kevin <trandzungck@gmail.com>
2025-06-23 16:36:02 +0700

    v

--- spark_jobs/job_transform_data.py
+++ spark_jobs/job_transform_data.py
@@ -1,9 +1,22 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
 import argparse
 
 # Setup logging
 from utils.logger import get_logger
 logger = get_logger(__name__)
+
+column_mapping = {
+    "ID": "ID",
+    "Club": "CLUB",
+    "Nationality": "NATIONALITY",
+    "Matches played": "MATCHES_PLAYED",
+    "Won": "WON",
+    "Drawn": "DRAWN",
+    "Lost": "LOST",
+    "Possesion (%)": "POSSESSION",
+    "Passing accuracy (%)": "PASSING_ACCURACY"
+}
 
 def transform_data (spark, input_path, output_path):
     logger.info(f'Reading raw data from {input_path}')
@@ -15,9 +28,15 @@ def transform_data (spark, input_path, output_path):
     logger.info('Preview of raw data:')
     df.show(5)
     
+    #Map the columns:
+    mapped_df = df.select([col(old).alias(new) for old, new in column_mapping.item()])
+    
+    logger.info('Transformed Data Preview:')
+    mapped_df.show(5)
+    
     # Combine all csv files to 1 single file
     logger.info(f'Combine files and write to {output_path}')
-    df.coalesce(1).write.mode('overwrite').option('header', True).csv(output_path)
+    mapped_df.coalesce(1).write.mode('overwrite').option('header', True).csv(output_path)
     
 def main():
     parser = argparse.ArgumentParser()
